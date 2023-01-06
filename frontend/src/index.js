251d3f3b1c3b3838b7bef4c2140c23bec1d840d3
Tristin <tmille2004@gmail.com>
2023-01-05 22:43:39 -0600

    implements redux-persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,18 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(store)
+
 root.render(
 	<Provider store={store}>
 		<Router>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Router>
 	</Provider>
 )
